f3ab3ca342cb7f20ca43ad841cbda1ac5846e89d
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-11-11 08:18:56 +0100

    Add card, container and globalstyle

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Card from "./components/Card";
+import Container from "./components/Container";
 import GlobalStyle from "./GlobalStyle";
 
 function App() {
@@ -5,27 +7,9 @@ function App() {
     <>
       <GlobalStyle />
 
-    <div class="container">
-        <div class="card">
-            <div class="sneaker">
-                <div class="circle"></div>
-                <img src="./adidas.png" alt="adidas">
-            </div>
-            <div class="info">
-                <h1 class="title">Adidas ZX</h1>
-                <h3>FUTURE-READY TRAINERS WITH WRAPPED BOOST FOR EXCEPTION COMFORT.</h3>
-                <div class="sizes">
-                    <button>39</button>
-                    <button>40</button>
-                    <button class="active">42</button>
-                    <button>44</button>
-                </div>
-                <div class="purchase">
-                    <button>Purchase</button>
-                </div>
-            </div>
-        </div>
-    </div>
+      <Container>
+        <Card></Card>
+      </Container>
     </>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  transform-style: preserve-3d;
+  min-height: 80vh;
+  width: 35rem;
+  border-radius: 30px;
+  padding: 0rem 5rem;
+  box-shadow: 0 20px 20px rgba(0, 0, 0, 0.2), 0px 0px 50px rgba(0, 0, 0, 0.2);
+`;
+
+export default Card;
